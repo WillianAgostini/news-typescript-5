efe65e11967ad4090ba120feca3435635ed7c247
Willian Agostini <willian.agostini@gmail.com.br>
2023-06-19 16:08:34 -0300

    log example

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { log } from './decorators/log';
+
+class Calculator {
+    @log
+    add(a: number, b: number): number {
+        return a + b;
+    }
+}
+
+const calculator = new Calculator();
+console.log(calculator.add(2, 3));
